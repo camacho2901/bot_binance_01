c598ada859691a452b9cf6776c38eb5d4c4de8ef
camacho2901 <42523340+camacho2901@users.noreply.github.com>
2025-06-02 12:56:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -40,5 +40,5 @@ def enviar_mensaje():
         return jsonify({"estado": "error", "mensaje": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
