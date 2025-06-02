16b0534287507684c64f6ecb38dd143235a3a247
camacho2901 <42523340+camacho2901@users.noreply.github.com>
2025-06-02 12:05:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,8 @@ def bot():
                     if (tipo == "BUY" and precio <= limite) or (tipo == "SELL" and precio >= limite):
                         nombre = ofertas[0]["advertiser"]["nickName"]
                         enlace = f"https://p2p.binance.com/es/advertiserDetail?advertiserNo={ofertas[0]['advertiser']['userNo']}"
-                        msg = f"💱 *{tipo}* oferta de *{moneda}*"
+                        msg = f"💱 *{tipo}* oferta de *{moneda}*\n👤 Vendedor: {nombre}\n💰 Precio: *{precio} Bs.*\n🔗 [Ver Oferta]({enlace})"
+                        enviar_mensaje(msg)
 👤 Vendedor: {nombre}
 💰 Precio: *{precio} Bs.*
 🔗 [Ver Oferta]({enlace})"
